f74e107e297a02d54b46985207458d3141d6d93a
dressupdarling <yevgenyokun2@gmail.com>
2025-11-12 10:54:36 +0000

    900 Secret number
    Implement main function to read input and process secret numbers.

--- 900/Secret_number.py
+++ 900/Secret_number.py
@@ -1,1 +1,24 @@
-
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    
+    t = int(data[0])
+    index = 1
+    
+    for _ in range(t):
+        r = int(data[index])
+        index += 1
+        
+        d = 11
+        ans = []
+        while r >= d:
+            if r % d == 0:
+                ans.append(r // d)
+            d = (d - 1) * 10 + 1
+        
+        print(len(ans))
+        print(' '.join(map(str, ans[::-1])))
+ 
+if __name__ == "__main__":
+    main()
