6298d2c522bba81c70f5f7875688b8342032ab7f
dressupdarling <yevgenyokun2@gmail.com>
2025-11-12 09:25:30 +0000

    Code
    Implement logic to calculate the difference between sums based on conditions.

--- Preparing_for_the_Olympiad.py
+++ Preparing_for_the_Olympiad.py
@@ -1,1 +1,15 @@
-
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    m = 0
+    s = 0
+    for i in range(n):
+        if i == n - 1:
+            m += a[i]
+        else:
+            if a[i] - b[i + 1] > 0:
+                m += a[i]
+                s += b[i + 1]
+    print(m - s)
