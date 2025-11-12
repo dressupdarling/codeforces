76d285f365b89b271006a3afe3f812d699549cf8
dressupdarling <yevgenyokun2@gmail.com>
2025-11-12 09:33:43 +0000

    Add function to calculate candies to add

--- Candies_for_Nephews.py
+++ Candies_for_Nephews.py
@@ -1,1 +1,13 @@
-
+def candies_add_x():
+    n_candies = int(input())
+    add_x = (n_candies%3)
+    if add_x == 1:
+        print(2)
+    elif add_x == 2:
+        print(1)
+    else:
+        print(0)
+ 
+t = int(input())
+for _ in range(t):
+    candies_add_x()
