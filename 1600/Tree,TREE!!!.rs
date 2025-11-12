1e48aee053410ff99db0313332ccb9d2e595bed9
dressupdarling <yevgenyokun2@gmail.com>
2025-11-12 19:16:08 +0000

    comments

--- 1600/Tree,TREE!!!.rs
+++ 1600/Tree,TREE!!!.rs
@@ -5,26 +5,26 @@ fn main() {
   io::stdin().read_to_string(&mut input).unwrap();
   let mut it = input.split_whitespace();
 
+  
   for case in 0..t {
     let n: usize = it.next().unwrap().parse().unwrap();
     let m: usize = it.next().unwrap().parse().unwrap()'
     let mut adj_lst = vec![Vec::new; n+1]; // Adjacency list
     for _ in 0..n - 1 {
-            let u: usize = it.next().unwrap().parse().unwrap();
-            let v: usize = it.next().unwrap().parse().unwrap();
-            adj[u].push(v);
-            adj[v].push(u);
+          let u: usize = it.next().unwrap().parse().unwrap();
+          let v: usize = it.next().unwrap().parse().unwrap();
+          adj[u].push(v);
+          adj[v].push(u);
         }
     print!("{}", out);
 }  
 
   let (_parent, mut sub) = dfs1(n, &adj);
-
         let f_root = compute_root(n, k, &sub);
-
         let mut ans = vec![0i64; n + 1];
         ans[1] = f_root;
         dfs2_iterative(1, 0, n, k, &adj, &mut sub, &mut ans);
+
 
         let total = final_sum(&ans);
         out.push_str(&format!("{}\n", total));
