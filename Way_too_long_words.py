47ec1c19428798be5caff67d3884d3a720f14c21
dressupdarling <yevgenyokun2@gmail.com>
2025-11-12 10:07:19 +0000

    Add way_to_long function for word abbreviation
    Implement function to abbreviate long words

--- Way_too_long_words.py
+++ Way_too_long_words.py
@@ -1,1 +1,25 @@
-
+def way_to_long():
+    n = int(input())  
+ 
+    for _ in range(n):
+        word = input()
+        length_of_word = len(word)
+ 
+        if length_of_word > 10:
+ 
+            list_word = list(word)
+            letter_first = list_word[0]
+            letter_last = list_word[-1]
+ 
+            list_word.pop(0)
+            list_word.pop(-1)
+ 
+            new_word = "".join(list_word)
+            length_new_word = len(new_word)
+            abbr = letter_first + str(length_new_word) + letter_last
+            print(abbr)
+ 
+        else:
+            print(word)
+ 
+way_to_long()
