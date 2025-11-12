e4ebf463277f872dabec0948c19413b35a718408
dressupdarling <yevgenyokun2@gmail.com>
2025-11-12 09:34:28 +0000

    Add max_turns function to calculate maximum turns

--- Catching_krug.py
+++ Catching_krug.py
@@ -1,1 +1,16 @@
-
+def max_turns(n, x_k, y_k, x_d, y_d):
+    ans = 0
+    if x_d > x_k:
+        ans = max(ans, x_d)
+    elif x_d < x_k:
+        ans = max(ans, n - x_d)
+    if y_d > y_k:
+        ans = max(ans, y_d)
+    elif y_d < y_k:
+        ans = max(ans, n - y_d)
+    return ans
+ 
+t = int(input())
+for _ in range(t):
+    n, x_k, y_k, x_d, y_d = map(int, input().split())
+    print(max_turns(n, x_k, y_k, x_d, y_d))
