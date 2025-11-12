47b504e9972bf407def549c3e5140eabb27676a5
dressupdarling <yevgenyokun2@gmail.com>
2025-11-12 09:29:42 +0000

    Add max_rating function to process rating rounds
    Implement max_rating function to calculate rating changes based on rounds.

--- Round_trip.py
+++ Round_trip.py
@@ -1,1 +1,17 @@
-
+def max_rating(R,X,D,rounds):
+    rating = R
+    count = 0
+    for i in rounds:
+        if i == "1":
+            count += 1
+            rating = max(0, rating - D)
+        else:
+            if rating < X:
+                count += 1
+    return count
+    
+t = int(input())
+for _ in range(t):
+    R,X,D,n = map(int,input().split())
+    rounds = input()
+    print(max_rating(R,X,D,rounds))
