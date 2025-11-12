c75c2d86169c460bd3dbaaa4adac7fe94fb64ce7
dressupdarling <yevgenyokun2@gmail.com>
2025-11-12 18:58:24 +0000

    adjacency list initialization and improve output

--- 1600/Tree,TREE!!!.rs
+++ 1600/Tree,TREE!!!.rs
@@ -12,9 +12,29 @@ fn main() {
   // Adjacency list
     
     let mut adj_lst = vec![Vec::new; n+1];
-     for _ in 0..n - 1 {
+    for _ in 0..n - 1 {
             let u: usize = it.next().unwrap().parse().unwrap();
             let v: usize = it.next().unwrap().parse().unwrap();
             adj[u].push(v);
             adj[v].push(u);
         }
+
+
+    print!("{}", out);
+}  
+
+  let (_parent, mut sub) = dfs1(n, &adj);
+
+        let f_root = compute_root(n, k, &sub);
+
+        let mut ans = vec![0i64; n + 1];
+        ans[1] = f_root;
+        dfs2_iterative(1, 0, n, k, &adj, &mut sub, &mut ans);
+
+        let total = final_sum(&ans);
+        out.push_str(&format!("{}\n", total));
+    }
+
+    print!("{}", out);
+}
+
